1329f3a8011dc84fc4b8d5be972d832d9f8e9fde
Nenad Paic <nenadpaic@gmail.com>
2017-03-22 16:47:27 +0100

    Config with tests

---
+++ tests/class-inittest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Filter_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class InitTest extends WP_UnitTestCase {
+
+	/**
+	 * Covers configuration in filter-plugin.php file
+	 */
+	function test_config() {
+
+		$config = get_config();
+
+		$this->assertEquals( array(
+			'version' => 0.1,
+		), $config, 'Config is not right' );
+	}
+}
