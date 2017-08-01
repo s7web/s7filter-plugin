2de5f3fe125ba9ba3ca2de32786cdd0066c2f661
s7npaic <npaic@s7designcreative.com>
2017-08-01 11:45:51 +0200

    Register rest of scripts

--- inc/class-plugin.php
+++ inc/class-plugin.php
@@ -10,6 +10,7 @@
 namespace S7designFilter;
 
 use S7designFilter\Admin\Settings;
+use S7designFilter\Front\FrontController;
 
 /**
  * Class Plugin
@@ -51,6 +52,14 @@ class Plugin {
 			add_action( 'admin_post_otrs_style_settings', array( $admin_settings, 'save_general_settings' ) );
 			add_action( 'admin_post_otrs_save_page_settings', array( $admin_settings, 'save_page_settings' ) );
 		}
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'admin_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'front_end_scripts' ) );
+		$front_controller = new FrontController( $this->config );
+		add_action( 'template_include', array( $front_controller, 'template_redirect' ), 99 );
+		add_action( 'wp_ajax_ot_api_data', array( $front_controller, 'provide_data' ) );
+		add_action( 'wp_ajax_nopriv_ot_api_data', array( $front_controller, 'provide_data' ) );
 	}
 
 	/**
@@ -79,12 +88,50 @@ class Plugin {
 			'ot_interface_handler',
 			'ot_interface',
 			array(
-				'no_pages' => __( 'Sorry but there is no configured pages right now!', 'otrs-filter' ),
-				'is_pages' => $is_pages_setting,
+				'no_pages'   => __( 'Sorry but there is no configured pages right now!', 'otrs-filter' ),
+				'is_pages'   => $is_pages_setting,
 				'empty_name' => __( 'Page name can not be empty', 'otrs-filter' ),
-				'less_name' => __( 'This must be page id, start typing then select result from autocomplete', 'otrs-filter' ),
+				'less_name'  => __( 'This must be page id, start typing then select result from autocomplete',
+					'otrs-filter' ),
 			)
 		);
+	}
+
+	public function front_end_scripts() {
+		wp_enqueue_script(
+			'react-js',
+			$this->config->js_path . 'react.js',
+			false,
+			'1',
+			false
+		);
+		wp_enqueue_script(
+			'ot_filter_component',
+			$this->config->js_path . 'filter_component_js/filter_component.js',
+			array( 'react-js', 'jquery', 'underscore' ),
+			'1',
+			true
+		);
+		wp_localize_script( 'ot_filter_component',
+			'filter_objects',
+			array(
+				'ajax_url'     => admin_url( 'admin-ajax.php' ),
+				'page_id'      => get_the_ID(),
+				'title_cat'    => __( 'Countries', 'otrs-filter' ),
+				'title_tag'    => __( 'Sectors', 'otrs-filter' ),
+				'used_filters' => __( 'Used filters', 'otrs-filter' ),
+			)
+		);
+	}
+
+	/**
+	 * Add styles for admin UI
+	 *
+	 * @return void
+	 */
+	public function admin_styles() {
+
+		wp_enqueue_style( 'otrs-filter-style', $this->config->css_path . 'admin_interface.css' );
 	}
 
 }
