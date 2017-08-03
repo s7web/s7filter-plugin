d95cb84832f968731276d7c15b52af4a310e9c4b
s7npaic <npaic@s7designcreative.com>
2017-08-03 13:33:05 +0200

    done some refactor

--- inc/templates/filter_template.php
+++ inc/templates/filter_template.php
@@ -3,11 +3,11 @@ get_header();
 $page_id = get_the_ID();
 
 ?>
-	<div id="content" class="site-content otrs-faq otrs-page">
+	<div id="content" class="site-content s7-faq s7-page">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12">
-					<div id="ot_filters_display">
+					<div id="s7_filters_display">
 
 					</div>
 				</div>
